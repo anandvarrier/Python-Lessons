deef0bbf7805d22b7e6784fcd4cf0edac366bf3c
Anand Varrier <anand.v@sphinxworldbiz.com>
2023-06-09 19:08:33 +0530

    First commit

---
+++ madlib.py
@@ -0,0 +1,8 @@
+# A program to elaborate f string literals
+
+name = input('Enter a name: ')
+age = int(input('Enter age: '))
+act = input('Enter any activity: ')
+adj = input('Enter an Adjective: ')
+madlib = f"My name is {name}. \n I am {age} years old. \n I love {act}. \n It makes me {adj}."
+print(madlib)
