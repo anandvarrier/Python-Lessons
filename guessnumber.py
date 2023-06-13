1ee9091f7d84b9c31f8b9fd09fe6a2af9c5941e1
Anand Varrier <anand.v@sphinxworldbiz.com>
2023-06-13 18:45:43 +0530

    Second commit updated

--- guessnumber.py
+++ guessnumber.py
@@ -21,4 +21,30 @@ def guess():
     return f'You have guessed the right number which is {number}'
 
 
-print(guess())
+# print(guess())
+
+
+def computer_guess():
+    # low = 'l'
+    # high = 'h'
+    # correct = 'c'
+
+    x = int(input('Enter a number for the computer to guess: '))
+    print('Now, its time for you to guess the number that I have given.')
+    print('Let me give you a range for you to guess quickly.')
+    lower = int(input('Lower limit of range: '))
+    higher = int(input('Higher limit of range: '))
+    print(f'The range of numbers for you to guess is in between {lower} and {higher}')
+
+    r_num = random.randint(lower, higher + 1)  # generating a random number
+
+    while x != r_num:
+        if x > r_num:
+            print('Wrong guess. Try again')
+        elif x < r_num:
+            print('Wrong guess. Try again.')
+        r_num = random.randint(lower, higher + 1)
+    return 'You got the answer correct'
+
+
+print(computer_guess())
